9372d9d7aeddf31ad0a5e97d181402300f0a66ef
Jamie <jamienicole923@gmail.com>
2023-03-31 10:46:23 -0700

    Initial commit: ST HELENS ELECTRIC BIKE REPAIR SHEBIKE APP

---
+++ mobile/src/main/java/com/example/sdk2apk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sdk2apk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
